dee6e83dfc9c7f7425d759b867543e9ae6814b07
Sukumar Karthigeyan <ysukusuper@gmail.com>
2021-03-31 20:35:58 +0200

    Added the second button to the start page

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -2,9 +2,17 @@ import './Start.css';
 import React from 'react';
 import backgroundVideo from './video.mp4';
 import { Button } from '@material-ui/core';
+import axios from 'axios';
 
 function Start(props) {
-    const getStarted = () => {
+    const getStarted2D = () => {
+        axios.post('http://localhost:3001/loaddocker', { Dimensions: 2 });
+
+        props.stepChanged(0); // Switch to the landing page.
+    }
+
+    const getStarted2_5D = () => {
+        axios.post('http://localhost:3001/loaddocker', { Dimensions: 2.5 });
         props.stepChanged(0); // Switch to the landing page.
     }
 
@@ -14,11 +22,17 @@ function Start(props) {
                 <source src={backgroundVideo} type="video/mp4" />
             </video>
 
-            <div id="center-horizontally-vertically">
+            <div id="center-horizontally-vertically2D">
                 <form className="my-form">
                     <h1>Welcome!</h1>
                     <h1>3D object tracking in industrial environments made easier</h1>
-                    <Button id="button-get-started" onClick={getStarted}> GET STARTED </Button>
+                    <Button id="button-get-started" onClick={getStarted2D}> GET STARTED with 2D</Button>
+                </form>
+            </div>
+
+            <div id="center-horizontally-vertically2.5D">
+                <form className="my-form">
+                    <Button id="button-get-started" onClick={getStarted2_5D}> GET STARTED with 2.5D</Button>
                 </form>
             </div>
 
