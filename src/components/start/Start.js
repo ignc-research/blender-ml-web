cb09c489c89ec64c53d784554c641da3e6e076f1
Subhin Lee <leesubhin91@gmail.com>
2021-04-19 19:52:18 +0200

    redesigned the start page by adding information about tech specs and how it works

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -19,23 +19,82 @@ function Start(props) {
     }
 
     return (
-        <div className="Start">
-            <video autoPlay muted loop id="video">
-                <source src={backgroundVideo} type="video/mp4" />
-            </video>
+             
 
-            <div id="center-horizontally-vertically">
+        <div className="Start">
+
+            
+            <div className="header">
+                <h1>3D object tracking in industrial environments gets easier here!</h1>
+                <p class="expanded__text">We bring you a simple and great interface where you can set up basic configuration for the desired neural network weight. 
+                A few clicks will automate the process in generating data for 6D pose estimation. Boost your data generation process here!</p>
+            </div>
+
+       
+            <div className="about">
+                <h2>Tech specs</h2>
+
+                <div className="tile-container">
+                    <div className="tile">
+                        <div className="tile-image">
+                            <img src="/images/blender.png"></img>
+                        </div>
+                        <div className="tile-text">
+                            Generate labeled images using blender script automatically.
+                        </div>
+                    </div>
+                    <div className="tile">
+                        <div className="tile-image">
+                            <img src="/images/artificial-intelligence.png"></img>
+                        </div>
+                        <div className="tile-text">
+                            6D pose estimation will get trained based on user's configuration.
+                        </div>
+                    </div>
+                    <div className="tile">
+                        <div className="tile-image">
+                            <img src="/images/annotate.png"></img>
+                        </div>
+                        <div className="tile-text">
+                            Possible to add annotated dataset for neural weight using Annotate.photo 
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="work-flow">
+                 <h2>How it works</h2>
+                 <div className="howitworks-image">
+                     <img src="/images/howitworks.png"></img>
+                 </div>
+            </div>
+
+            <div className="get-started-section">
+                <h2>Let's get started!</h2>
+                <p>Please choose one option for your training dataset</p>
+                <div className="button-container">
+                <Button variant="outlined" size="large" color="secondary" id="button-get-started" onClick={getStarted2D}> 2D</Button>
+                <Button variant="contained" size="large" color="secondary" id="button-get-started" onClick={getStarted2_5D}>  2.5 D</Button>
+                </div>
+
+               
+            </div>
+           
+                
+
+            {/* <div id="center-horizontally-vertically">
                 <form className="my-form">
                     <h1>Welcome!</h1>
                     <h1>3D object tracking in industrial environments made easier</h1>
                     <Button id="button-get-started" onClick={getStarted2D}> GET STARTED with 2D</Button>
                     <Button id="button-get-started" onClick={getStarted2_5D}> GET STARTED with 2.5D</Button>
                 </form>
-            </div>
+            </div> */}
 
-            {/*TODO: Give maybe more information about the website - what it does, how it works. */}
-            
+           {/*TODO: Give maybe more information about the website - what it does, how it works. */}
         </div>
+            
+            
     );
 }
 
